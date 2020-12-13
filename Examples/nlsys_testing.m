d2a2ac03735935211bb0d4b4be6581b4ecb94515
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2020-12-13 04:35:29 -0600

    Further developed methods of nlsys class: definitions and standard update methods

--- Examples/nlsys_testing.m
+++ Examples/nlsys_testing.m
@@ -2,13 +2,42 @@
 % Jonas Wagner 2020-12-11
 
 
-sys = nlsys(@nonlin_func)
+sys = nlsys(@nonlin_state_func)
+
+x_0 = [1,2];
+
+sys2 = nlsys(@nonlin_state_func,@nonlin_output_func,x_0)
 
 
+u_test = 1;
+t_test = 1;
+sys2.dx(u_test)
+sys3 = sys2.update(u_test,t_test)
+x = sys3.x
 
 % Local Functions
-function y = nonlin_func(x,u)
+function y = nonlin_state_func(x,u)
     % NONLIN_FUNC Example function for use with nlsys based simulation
+    arguments
+        x (2,1) = [0; 0];
+        u (1,1) = 0;
+    end
+    
+    % Array sizes
+    n = 2; % Number of states
+    p = 1; % Number of inputs
+
+    % State Update Equations
+    y(1,1) = 2 * x(1) + x(2) * u;
+    y(2,1) = x(1) * x(2);
+    
+    if nargin ==0
+        y = [n;p];
+    end
+end
+
+function y = nonlin_output_func(x,u)
+    % NONLIN_OUTPUT_FUNC Example function for use with nlsys
     arguments
         x (2,1) = [0; 0];
         u (1,1) = 0;
@@ -19,12 +48,9 @@ function y = nonlin_func(x,u)
     p = 1; % Number of inputs
     q = 2; % Number of outputs
     
-    % Correct type and size of output array
-%     y = type(x).empty(n,1);
-    
     % State Update Equations
-    y(1) = 2 * x(1) + x(2) * u;
-    y(2) = x(1) * x(2);
+    y(1,1) = x(1) + u;
+    y(2,1) = x(2) * u;
     
     if nargin ==0
         y = [n;p;q];
--- base/nlsys.m
+++ base/nlsys.m
@@ -26,35 +26,134 @@ classdef nlsys
     methods
         function sys = nlsys(f, h, x)
             % Constructor for nlsys... 
-            % f is state eq... must be of correct size, shape etc.
-            % h is output eq (optional)
-            % x is the current state (optional)
             arguments
-                f (:,1)
-                h       = @(x,u) x;
-                x (:,1) = -1;
+                % f is state eq (or nlsys or lti object)
+                f
+                % h is output eq (optional) default = output just x
+                h               = @h_default
+                % x is the current state (optional) default = 0
+                x (:,1) double = -1;
             end
+            
+            % State Equation
+            
+%             if type(f) == nlsys
+%                 f = f.f;
+%             end
             sys.f = f;
             
-            temp = f();
-            sys.n = temp(1);
-            sys.p = temp(2);
-            sys.q = temp(3);
+            % System Size
+            temp = f(); n = temp(1); p = temp(2);
+            sys.n = n;
+            sys.p = p;
             
+            % Output Equation
             sys.h = h;
-            if x == -1
+            
+            % Output Size
+            if nargin >= 2
+                temp = h(); %temp(1) = n, temp(2) = p, temp(3) = q
+                if temp(1) == -1
+                    temp(1) = sys.n;
+                end
+                if temp(2) == -1
+                    temp(2) = sys.p;
+                end
+                if temp(1) ~= sys.n || temp(2) ~= sys.p
+                    error('h() is not compatible with f()')
+                end
+                sys.q = temp(3);
+            else
+                sys.q = sys.n;
+            end
+            
+            % System State
+            if x == -1 %either no input given for x or none specified w/ -1
                 x = zeros(sys.n,1);
             end
+            if size(x,1) ~= sys.n
+                error('x incorrect size')
+            end
             sys.x = x;
+            
+            
+            % Validization
+            try
+                x_test = sys.f(x,zeros(sys.p,1));
+            catch
+                error('f(x,0) does not work')
+            end
+            if size(x_test,1) ~= sys.n
+                error('f(x,0) incorrect size')
+            end
+            try
+                y_test = sys.h(x,zeros(sys.p,1));
+            catch ME
+                rethrow(ME)
+                error('h(x,0) does not work')
+            end
+            if sys.q ~= -1 && size(y_test,1) ~= sys.q
+                error('h(x,0) incorrect size')
+            end
+            
         end
         
         
-        % Standar System Operation
-%         function  = StateUpdate
+        % Standard System Operation
+        function dx = dx(sys,u,x)
+            % DX - returns state update eq (dx)
+            arguments
+                % sys is the nonlin sys
+                sys
+                % u is the input
+                u
+                % x is the current state
+                x = sys.x
+            end
+            dx = sys.f(x,u);
+        end
         
-        
-        
-        
+        function y = y(sys,u,x)
+            arguments
+                % sys is the nonlin sys
+                sys
+                % u is the input
+                u
+                % x is the current state
+                x = sys.x
+            end
+            y = sys.h(x,u);
+        end
+            
+        function  sys = update(sys,u,t,x)
+            % UPDATE - return an updated system based on u and t...
+            % and x (optional)
+            arguments
+                % sys is the nonlin sys
+                sys
+                % u is the input
+                u (:,1) double
+                % t is the time step to be updated by (assumign CT)
+                t           double
+                % x is the current state (optional) default = sys.x
+                x (:,1) double = sys.x
+            end
+            
+            % Input Validation
+            if size(u,1) ~= sys.p
+                error('u incorrect size')
+            end
+            if size(x,1) ~= sys.n
+                error('x incorrect size')
+            end
+            
+            % Update Equation
+            x = x + sys.dx(u,x) * t;
+            
+            % New sys definition
+            sys = nlsys(sys.f,@h_default,x);
+        end
+
         
         % Data Export
         function [f, h, x, n, p, q] = nlsysdata(sys)
@@ -66,9 +165,37 @@ classdef nlsys
             p = sys.p;
             q = sys.q;
         end
-        
-        
-        
     end
 end
 
+
+function y = h_default(x,u)
+    % H_DEFAULT default h function
+    arguments
+        x = 0;
+        u = 0;
+    end
+    % Simplistic output eq
+    y = x;
+    
+    % Exception n,p,and q... for verification steps
+    if nargin ==0
+        y = [-1;-1;-1];
+    end
+end
+
+function isLTI = IsLTI(sys)
+    % ISLTI tests if a system is lti
+    try
+        ss = strcmp(class(sys), 'ss');
+        tf = strcmp(class(sys), 'tf');
+        zpk = strcmp(class(sys), 'tf');
+        if ss || tf || zpk
+            isLTI = true;
+        else
+            isLIT = false;
+        end
+    catch
+        isLTI = false;
+    end
+end
